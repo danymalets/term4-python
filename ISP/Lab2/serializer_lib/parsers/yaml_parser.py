f79b8bdf37255c43cc2054caadafbe81399a9401
danymalets <malets.d.l@gmail.com>
2021-05-21 23:40:45 +0300

    yaml parser

--- ISP/Lab2/serializer_lib/parsers/yaml_parser.py
+++ ISP/Lab2/serializer_lib/parsers/yaml_parser.py
@@ -1,97 +1,91 @@
-from yaml import dump as dumps, full_load as loads
-
-
 import re
 
 FLOAT_REGEX = "-?\d+\.\d+"
 INT_REGEX = "\d+"
-STR_REGEX = "\"(.*)\""
-DICT_REGEX = "\{([\s\S]*)\}"
-LIST_REGEX = "\[([\s\S]*)\]"
+STR_REGEX = "\'(.*)\'"
+KEY_VAL_REGEX = "(.*):([\s\S]*)"
 
 
 def to_yaml(obj, indent=0):
     if obj is None:
-        return ""
+        return "null"
     elif isinstance(obj, bool):
         return str(obj).lower()
     elif isinstance(obj, (int, float)):
         return str(obj)
     elif isinstance(obj, str):
-        return "\'" + obj + "\'"
+        return "\'" + obj.replace('\\', '\\\\').replace('\n', '\\n') + "\'"
     elif isinstance(obj, list):
-        res = "\n"
-        for o in obj:
-            res += f"{' ' * indent}- {to_yaml(o, indent + 2)}\n"
-        return res[:-1]
+        if not obj:
+            return "[]"
+        else:
+            res = ""
+            for o in obj:
+                res += f"{' ' * indent}- {to_yaml(o, indent + 2)}\n"
+            return res[indent:-1]
     elif isinstance(obj, dict):
         res = ""
+        if not obj:
+            return "{}"
         for key, val in obj.items():
-            res += f"{' ' * indent}{to_yaml(key)}: {to_yaml(val, indent + 2)}\n"
+            str_val = to_yaml(val, indent + 2)
+            if isinstance(val, (list, dict)) and val:
+                str_val = "\n" + " " * (indent + 2) + str_val
+            res += f"{' ' * indent}{to_yaml(key)}: {str_val}\n"
         return res[indent:-1]
     else:
         raise ValueError(f"Wrong type {type(obj)}")
 
 
-def from_yaml(s):
+def from_yaml(s, indent=0):
     s = s.strip("\n ")
     if s == "null":
         return None
+    elif s == "[]":
+        return []
+    elif s == "{}":
+        return {}
     elif s == "false" or s == "true":
         return s[0] == 't'
     elif re.fullmatch(INT_REGEX, s):
         return int(s)
     elif re.fullmatch(FLOAT_REGEX, s):
         return float(s)
-    elif re.fullmatch(DICT_REGEX, s):
-        a = {}
-        for ss in split(re.fullmatch(DICT_REGEX, s).group(1), ','):
-            key, value = tuple(split(ss, ':'))
-            a[from_json(key)] = from_json(value)
-        return a
-    elif re.fullmatch(LIST_REGEX, s):
-        return [from_json(ss) for ss in split(re.fullmatch(LIST_REGEX, s).group(1), ',')]
     elif re.fullmatch(STR_REGEX, s):
         return re.fullmatch(STR_REGEX, s).group(1).replace('\\n', '\n').replace('\\\\', '\\')
     else:
-        raise ValueError(f"Wrong string \"{s}\"")
+        a = split(s, indent)
+        is_list = False
+        for i in range(len(a)):
+            a[i] = a[i].strip("\n ")
+            if a[i][0:2] == "- ":
+                is_list = True
+        if is_list:
+            res = []
+        else:
+            res = {}
+        for s in a:
+            if is_list:
+                res.append(from_yaml(s[2:], indent + 2))
+            else:
+                m = re.fullmatch(KEY_VAL_REGEX, s)
+                if m is None:
+                    raise ValueError(f"Wrong string \"{s}\"")
+                key = m.group(1)
+                val = m.group(2)
+                res[from_yaml(key)] = from_yaml(val, indent + 2)
+        return res
 
 
-def split(s, mark):
+def split(s, indent):
     a = []
-    depth = 0
     tmp = ""
-    in_str = False
-    marks = 0
     for i in range(len(s)):
-        if is_quotation(s, i):
-            in_str = not in_str
-
-        if not in_str:
-            if s[i] == '[' or s[i] == '{':
-                depth += 1
-            elif s[i] == ']' or s[i] == '}':
-                depth -= 1
-
-        if s[i] == mark and depth == 0 and not in_str:
+        if i+indent+1 < len(s) and s[i] == "\n" and s[i+1:i+indent+1] == " " * indent and s[i+indent+1] != " ":
             a.append(tmp)
             tmp = ""
-            marks += 1
         else:
             tmp += s[i]
-
-    if tmp.strip("\n ") != "":
-        a.append(tmp)
-
+    a.append(tmp)
     return a
 
-
-def is_quotation(s, ind):
-    if s[ind] != "\"":
-        return False
-    ind -= 1
-    cnt = 0
-    while ind >= 0 and s[ind] == "\\":
-        ind -= 1
-        cnt += 1
-    return cnt % 2 == 0
