53f01ecdb45090629bd88f4ac801211f0641310a
danymalets <malets.d.l@gmail.com>
2021-05-22 22:17:59 +0300

    toml

--- ISP/Lab2/serializer_lib/parsers/toml_parser.py
+++ ISP/Lab2/serializer_lib/parsers/toml_parser.py
@@ -2,6 +2,14 @@ import re
 from toml import dumps, loads
 
 
+import re
+
+FLOAT_REGEX = "-?\d+\.\d+"
+INT_REGEX = "\d+"
+STR_REGEX = "\"(.*)\""
+LIST_REGEX = "\[([\s\S]*)\]"
+
+
 def to_toml(obj):
     return dumps(obj)
 
