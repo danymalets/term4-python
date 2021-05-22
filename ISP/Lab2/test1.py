af4a1a3f84b9c64814e7a1fce515aa6cac360e75
danymalets <malets.d.l@gmail.com>
2021-05-22 22:18:37 +0300

    final tests

--- ISP/Lab2/test1.py
+++ ISP/Lab2/test1.py
@@ -2,16 +2,7 @@ from serializer_lib.factory.factory import create_serializer
 from tests.test_main import *
 
 
-from serializer_lib.parsers.yaml_parser import to_yaml, from_yaml
-from serializer_lib.parsers.json_parser import from_json
 
-
-def f(x):
-    return x*x
-
-
-ser = create_serializer("json")
-ser.dump(f, "my_tests/func.json")
 
 
 
--- ISP/Lab2/test2.py
+++ ISP/Lab2/test2.py
@@ -1,3 +1,14 @@
 from serializer_lib.factory.factory import create_serializer
-import logging
 
+ser = create_serializer("json")
+d = ser.load("my_tests/z1.json")
+fib = ser.load("my_tests/z2.json")
+func = ser.load("my_tests/z3.json")
+A = ser.load("my_tests/z4.json")
+a = ser.load("my_tests/z5.json")
+
+print(d)
+print(fib(5))
+print(func(1))
+print(A())
+print(a.sqr(5))
